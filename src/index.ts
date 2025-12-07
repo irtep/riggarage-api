c394c1fa4be437d8bafd38557f9ee3372f32185d
irtep <rasanen.petri@gmail.com>
2025-12-07 19:54:15 +0200

    1.1.0

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import https from 'https';
 
 import authRoutes from './routes/auth';
 import rigsRoutes from './routes/rigs';
@@ -17,16 +19,35 @@ for (const envVar of requiredEnvVars) {
     }
 }
 
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '7d';
 
 const app = express();
-const PORT = process.env.PORT || 5509;
+const PORT = process.env.PORT || 5510;
 
 // Middleware
-app.use(cors({
-    origin: [/^https?:\/\/localhost(:\d+)?$/, /^https?:\/\/193\.28\.89\.151:5509$/] // both http and https for my ip and all localhosts
-}));
+app.use(
+    cors({
+        origin: [
+            // Local development
+            "http://localhost:3000",
+            "http://localhost:5509",
+            "http://127.0.0.1:3000",
+            "http://127.0.0.1:5509",
+
+            "https://irtep.github.io",
+
+            "https://irtep.github.io/riggarage"
+        ],
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    })
+);
 
 app.use(helmet());
 app.use(express.json());
@@ -35,7 +56,13 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/rigs', rigsRoutes);
 
+/*
 app.listen(PORT, () => {
-    console.log(`rig garage API, version 0.3.0`);
+    console.log(`rig garage API, version 0.4.1`);
     console.log(`Server running at http://localhost:${PORT}`);
+});
+*/
+https.createServer(options, app).listen(PORT, () => {
+  console.log('version 1.1.0');
+  console.log(`HTTPS server running on https://193.28.89.151:${PORT}`);
 });
